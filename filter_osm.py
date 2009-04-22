5f57f1429809125c5d78b4d932e5c7e22dec954d
jajcus <jajcus@3cb84372-2dac-11de-a1c6-114bfcb2beae>
2009-04-22 16:57:09 +0000

    - retrieve current data via API

--- filter_osm.py
+++ filter_osm.py
@@ -19,44 +19,38 @@
 
 
 """
-Czyta plik data/raw_data.osm i filtruje zapisując do data/data.osm tylko
-węzły mogące leżeć w Polsce.
+Reads data/raw_data.osm and retrieves ids of nodes witch may belong to Poland.
+Current versions of these nodes are retrieved by the official API then
+and writhundred to data/data.osm.
 """
 
 __version__ = "$Revision$"
 
 import subprocess
 import sys
-
-import locale,codecs
-locale.setlocale(locale.LC_ALL, "")
-encoding = locale.getlocale()[1]
-if not encoding:
-    encoding = "us-ascii"
-sys.stdout = codecs.getwriter(encoding)(sys.stdout, errors = "replace")
-sys.stderr = codecs.getwriter(encoding)(sys.stderr, errors = "replace")
-
-
+from teryt2osm.reporting import Reporting
+from teryt2osm.utils import setup_locale, add_to_list_dict, count_elements
 import xml.etree.cElementTree as ElementTree
+from urllib import urlopen
+
+setup_locale()
+reporting = Reporting(logging = False)
 
 # fast node count
-popen = subprocess.Popen(["grep", "-c", "<node", "data/raw_data.osm"],
-                                            stdout=subprocess.PIPE)
-(stdout, stderr) = popen.communicate()
-node_count = int(stdout.strip())
+node_count = count_elements("data/raw_data.osm", "node")
 step = node_count / 100
 
-print >>sys.stderr, "Loading data/raw_data.osm"
+reporting.output_msg("info", u"Ładuję data/raw_data.osm")
 tree = ElementTree.parse("data/raw_data.osm")
 
-print >>sys.stderr, "Filtering..."
+reporting.progress_start(u"Filtruję...", node_count)
+node_ids = []
 root = tree.getroot()
 i = 0
 # use list instead of iterator, so there is not problem with removing items
-for element in list(root):  
+for element in root:  
     if element.tag != 'node':
         print >>sys.stderr, "Removing %r element"  % (element.tag,)
-        root.remove(element)
     else:
         remove = False
         name = "unknown"
@@ -66,8 +60,6 @@ for element in list(root):
                 continue
             key = tag.attrib["k"]
             value = tag.attrib["v"]
-            if id == "240033907":
-                print >>sys.stderr, "Key: %r, Value: %r" % (key, value)
             if key in ('is_in', 'is_in:country'):
                 if "Poland" not in value and "Polska" not in value:
                     remove = True
@@ -79,9 +71,43 @@ for element in list(root):
                 name = value
         if remove:
             print >>sys.stderr, "Removing:", name
-            root.remove(element)
-    i += 1
-    if i % step == 0:
-        print >>sys.stderr, "%f%%" % (i * 100 / node_count)
+        else:
+            node_ids.append(str(id))
+    reporting.progress()
 
-tree.write("data/data.osm")
+reporting.progress_stop()
+
+del root
+
+node_ids.sort()
+
+NODES_PER_REQUEST=256
+
+reporting.progress_start(u"Pobieram aktualne wersje węzłów...", len(node_ids) / NODES_PER_REQUEST)
+root = ElementTree.Element(u"osm", version = u"0.6", generator = u"teryt2osm/filter_osm.py")
+tree = ElementTree.ElementTree(root)
+for i in xrange(0, len(node_ids), NODES_PER_REQUEST):
+    ids_set = node_ids[i:i+NODES_PER_REQUEST]
+    try:
+        data = urlopen('http://www.openstreetmap.org/api/0.6/nodes?nodes=' + ",".join(ids_set))
+        response = ElementTree.parse(data)
+        r_root = response.getroot()
+        for element in r_root:
+            root.append(element)
+    except IOError, err:
+        reporting.output_msg("info", u"Bulk download failed: %s" % (err,))
+        for node_id in ids_set:
+            try:
+                data = urlopen('http://www.openstreetmap.org/api/0.6/node/' + node_id)
+                response = ElementTree.parse(data)
+                r_root = response.getroot()
+                for element in r_root:
+                    root.append(element)
+            except IOError, err:
+                reporting.output_msg("errors", u"Błąd I/O podczas pobierania węzła %s: %s" 
+                                                    % (node_id, err))
+    reporting.progress()
+reporting.progress_stop()
+
+tree.write("data/data.osm", "utf-8")
+reporting.close()
